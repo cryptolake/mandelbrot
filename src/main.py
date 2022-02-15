357d66592a57f61de04321f63dcdb4b04e82c3f6
Nestor Fabian Villar <nesfvillar@gmail.com>
2022-02-15 03:11:48 -0300

    New coloring

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
-import math
-
 WIDTH = 512
 HEIGHT = 512
 MAX_COLOR = 255
-MAX_ITER = 200
+MAX_ITER = 50
 
 
-def colorize(idx: int) -> (int, int, int):
+def colorize(idx: int) -> (float, float, float):
     if idx == MAX_ITER:
-        return (0, 0, 0)
+        return (0.0, 0.0, 0.0)
     else:
-        r = min(int(abs(idx**2)), MAX_COLOR)
-        g = min(int(math.exp(idx)), MAX_COLOR)
-        b = min(int(math.cosh(idx)), MAX_COLOR)
+        x = idx / MAX_ITER
+        r = x**2
+        g = x
+        b = x**0.75
         return (r, g, b)
 
 
@@ -20,13 +19,13 @@ def mandelbrot(c: complex) -> int:
     f = 0
     for idx in range(MAX_ITER):
         f = f**2 + c
-        if abs(f) >= 2:
+        if abs(f) > 2:
             return idx
     return MAX_ITER
 
 
 def pixel_to_plane(i: int, j: int) -> complex:
-    k = 2.5
+    k = 2
     xo, yo = -1, 0
 
     x = k * (i/WIDTH - 0.5) + xo
@@ -35,13 +34,16 @@ def pixel_to_plane(i: int, j: int) -> complex:
 
 
 def main():
-    header = f'P3 {WIDTH} {HEIGHT}\n{MAX_COLOR}'
+    header = f'P3 {WIDTH} {HEIGHT}\n{MAX_COLOR}\n'
     with open('mandelbrot.ppm', 'w') as f:
         f.write(header)
         for j in range(HEIGHT):
             for i in range(WIDTH):
                 c = pixel_to_plane(i, j)
                 r, g, b = colorize(mandelbrot(c))
+                r = int(r * MAX_COLOR)
+                g = int(g * MAX_COLOR)
+                b = int(b * MAX_COLOR)
                 f.write(f' {r} {g} {b}\n')
 
 
